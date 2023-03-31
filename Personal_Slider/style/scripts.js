38186889619e023079145b8891998c2d3ee6220a
McCReuben <thingkr@gmail.com>
2023-03-31 18:11:18 -0400

    [main] fluff

--- Personal_Slider/style/scripts.js
+++ Personal_Slider/style/scripts.js
@@ -42,7 +42,6 @@ const nav = document.querySelector("nav");
 
 const handleNavToggle = () => {
   nav.dataset.transitionable = "true";
-
   nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
 };
 
